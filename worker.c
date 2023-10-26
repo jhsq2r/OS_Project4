06ed08e0d37dda580b7ee0b027357ae4836acf5d
jhsq2r <126211836+jhsq2r@users.noreply.github.com>
2023-10-26 10:43:18 -0500

    Update worker.c
    Added percentChance for outputs, still needs different random number generation for blocked and terminate

--- worker.c
+++ worker.c
@@ -16,6 +16,11 @@ typedef struct msgbuffer {
         int intData;
 } msgbuffer;
 
+int randomNum(int seed){
+        srand(seed);
+        return (rand() % 100) + 1;
+}
+
 int main(int argc, char** argv){
 
         msgbuffer receiver;
@@ -36,56 +41,57 @@ int main(int argc, char** argv){
 
         //printf("This is Child: %d, From Parent: %d, Seconds: %s, NanoSeconds: %s\n", getpid(), getppid(), argv[1], argv[2]);
 
-        int exitTime[2];
-        exitTime[0] = atoi(argv[1]) + sharedTime[0];
-        exitTime[1] = atoi(argv[2]) + sharedTime[1];
-        if (exitTime[1] >= 1000000000){
-                exitTime[1] -= 1000000000;
-                exitTime[0] += 1;
-        }
+        int blockedTime[2];
 
-        printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d JUST STARTING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
+        //printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d JUST STARTING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
 
-        int currentTime = sharedTime[0];
-        int secondsPassed = 0;
+        int percentChance;
+        int givenTime;
+        int incrementMe = 0;
         while (1){
+                incrementMe++;
                 //wait for message
                 if ( msgrcv(msqid, &receiver, sizeof(msgbuffer), getpid(), 0) == -1) {
                         perror("failed to receive message from parent\n");
                         exit(1);
                 }
                 printf("WORKER PID:%d Message received\n",getpid());
-                if (exitTime[0] > sharedTime[0] || exitTime[1] > sharedTime[1]){
-                        if(sharedTime[0] - exitTime[0] >= 2){
-                                break;
-                        }
-                        if(currentTime != sharedTime[0]){
-                                secondsPassed += sharedTime[0]-currentTime;
-                                printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d, %d SECONDS HAVE PASSED\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1],secondsPassed);
-                                currentTime = sharedTime[0];
-                        }
-                        //send message saying not terminating
-                        receiver.mtype = getppid();
-                        receiver.intData = 1;
-                        if (msgsnd(msqid, &receiver, sizeof(msgbuffer)-sizeof(long),0) == -1){
-                                perror("msgsnd to parent failed\n");
-                                exit(1);
-                        }
 
+                givenTime = receiver.intData;
+                percentChance = randomNum(sharedTime[1] + givenTime + incrementMe);
+                printf("PercentChance: %d\n", percentChance);
+                if (percentChance <= 5){//Blocked send percent of time used
+                        receiver.intData = 1;
+                }else if (percentChance > 5 && percentChance < 97){//Send all time back
+                        receiver.intData = 2;
+                }else if (percentChance >= 97 && percentChance <= 100){//Terminate send percent of time back
+                        receiver.intData = 3;
+                        break;
                 }else{
+                        printf("Something went wrong: Random number %d\n", percentChance);
+                        receiver.intData = 4;
                         break;
                 }
+
+                receiver.mtype = getppid();
+                if (msgsnd(msqid, &receiver, sizeof(msgbuffer)-sizeof(long),0) == -1){
+                        perror("msgsnd to parent failed\n");
+                        exit(1);
+                }
+
         }
-        printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d TERMINATING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
+
         receiver.mtype = getppid();
-        receiver.intData = 0;
         if (msgsnd(msqid, &receiver, sizeof(msgbuffer)-sizeof(long),0) == -1){
                 perror("msgsnd to parent failed\n");
                 exit(1);
         }
 
+        printf("Killing child %d\n", getpid());
+        //printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d TERMINATING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
         shmdt(sharedTime);
 
         return 0;
 
 }
+
