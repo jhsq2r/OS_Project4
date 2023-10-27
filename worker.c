f06330160b7e03c71b8c536110e15cd8abc42fa3
jhsq2r <126211836+jhsq2r@users.noreply.github.com>
2023-10-27 12:47:31 -0500

    Update worker.c

--- worker.c
+++ worker.c
@@ -6,6 +6,7 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <errno.h>
+#include <time.h>
 
 #define SHMKEY 55555
 #define PERMS 0644
@@ -15,10 +16,6 @@ typedef struct msgbuffer {
         long mtype;
         int intData;
 } msgbuffer;
-
-int randomNum(){
-        return (rand() % 100) + 1;
-}
 
 int main(int argc, char** argv){
 
@@ -40,26 +37,25 @@ int main(int argc, char** argv){
 
         //printf("This is Child: %d, From Parent: %d, Seconds: %s, NanoSeconds: %s\n", getpid(), getppid(), argv[1], argv[2]);
 
-        int blockedTime[2];
-
         //printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d JUST STARTING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
 
+        srand(time(NULL));
+        int seed = rand();
         int percentChance;
         int givenTime;
-        int incrementMe = 0;
         while (1){
-                incrementMe++;
+                seed++;
                 //wait for message
                 if ( msgrcv(msqid, &receiver, sizeof(msgbuffer), getpid(), 0) == -1) {
                         perror("failed to receive message from parent\n");
                         exit(1);
                 }
-                printf("WORKER PID:%d Message received\n",getpid());
+                //printf("WORKER PID:%d Message received\n",getpid());
 
                 givenTime = receiver.intData;
-                srand(sharedTime[1] + givenTime + incrementMe);
+                srand(seed*getpid());
                 percentChance = (rand() % 100) + 1;
-                printf("PercentChance: %d\n", percentChance);
+                //printf("PercentChance: %d\n", percentChance);
                 if (percentChance <= 5){//Blocked send percent of time used
                         receiver.intData = (rand() % (givenTime - 1)) + 1;
                 }else if (percentChance > 5 && percentChance < 97){//Send all time back
@@ -87,7 +83,7 @@ int main(int argc, char** argv){
                 exit(1);
         }
 
-        printf("Killing child %d\n", getpid());
+        //printf("Killing child %d\n", getpid());
         //printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d TERMINATING\n",getpid(),getppid(),sharedTime[0],sharedTime[1],exitTime[0],exitTime[1]);
         shmdt(sharedTime);
 
